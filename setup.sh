5516bfbfa3e0d48a5553a32d3cbe766b7dc1d5a6
Craig Waterman <craigwaterman@gmail.com>
2016-04-06 16:56:26 -0400

    Newer setup

--- setup.sh
+++ setup.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-#echo
-#echo "Installing Java..."
-#cd /usr/local
-#wget -nv -O jre-7u45-linux-x64.gz http://javadl.sun.com/webapps/download/AutoDL?BundleId=81812
-#tar -xf jre-7u45-linux-x64.gz
-#rm jre-7u45-linux-x64.gz
-#ln -s /usr/local/jre1.7.0_45/bin/java /usr/bin/java
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+wget -O - http://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+sudo bash -c "echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list"
+sudo apt-get update
 
-#echo
-#echo "Installing Neo4j..."
-#cd /etc
-#wget -nv http://dist.neo4j.org/neo4j-community-2.2.5-unix.tar.gz
-#tar -xf neo4j-community-2.2.5-unix.tar.gz
-#rm neo4j-community-2.2.5-unix.tar.gz
-#ln -s /etc/neo4j-community-2.2.5/bin/neo4j /usr/bin/neo4j
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y neo4j
 
-#echo
-#echo "Updating Neo4j Config..."
-#sed -i 's/#org\.neo4j\.server\.webserver\.address=0\.0\.0\.0/org.neo4j.server.webserver.address=0.0.0.0/' /etc/neo4j-community-2.2.5/conf/neo4j-server.properties
+sudo echo -e "\nneo4j   soft    nofile  80000\nneo4j   hard    nofile  80000" >> /etc/security/limits.conf
+sudo echo -e "\nsession    required   pam_limits.so" >> /etc/pam.d/su
 
-#echo
-#echo "Starting Neo4j..."
-#neo4j start
+echo "Open file limits have been set but will require a reboot to take effect."
+
+echo "Updating Neo4j Config..."
+sudo sed -i 's/#org\.neo4j\.server\.webserver\.address=0\.0\.0\.0/org.neo4j.server.webserver.address=0.0.0.0/' /etc/neo4j/neo4j-server.properties
+
+echo "Restarting Neo4j"
+sudo service neo4j-service restart
+
+
